0ebc5403bee0c20ed55a58dd25d5946c298c8739
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-15 19:04:09 +0300

    backend: add cors headers

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 require('./config/db.config')
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: '*'
+}));
 
 const authRoutes = require('./routes/auth.routes');
 app.use('/auth', authRoutes)

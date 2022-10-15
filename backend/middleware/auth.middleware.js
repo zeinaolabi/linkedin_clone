7e0af68e2abd90c31ce53c6c6b14f418a5f1de5b
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-15 14:33:44 +0300

    backend: add jwt middleware

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/users.model');
+
+const authMiddleware = async (request, response, next) => {
+    const token = request.headers.authorization.split(" ")[1];
+
+    if(!token) return response.status(401).json({message: "Unauthorized"})
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await User.findOne({email: decoded.email}).lean()
+        request.user = {...user, userType: 1};
+        next()
+
+    }catch(error){
+        return response.status(401).json({message: "Unauthorized"})
+    }
+}
+
+module.exports = authMiddleware;

4aae029d99aea7587ae06b902a9703b5b2875e53
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-16 12:15:01 +0300

    frontend: add navigation through header buttons

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,7 +1,29 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom';
 import './header.css';
 
 const Header = () =>{
+    const navigate = useNavigate();
+
+    const reload = ()=> {
+        window.location.reload()
+    }
+
+    const navigateToNotifications = ()=> {
+        navigate('/notifications');
+    };
+
+    const navigateToProfile = ()=> {
+        navigate('/profile');
+    };
+
+    const logout = (e)=> {
+        e.preventDefault();
+
+        localStorage.clear();
+        window.location.href = '/';
+    }
+
     return (
         <div className="header">
             <div className="search_section">
@@ -10,10 +32,10 @@ const Header = () =>{
             </div>
 
             <ul className="header_list">
-                <li><img src="https://cdn-icons-png.flaticon.com/512/71/71200.png"></img>Jobs</li>
-                <li><img src="https://cdn-icons-png.flaticon.com/512/565/565422.png"></img>Notifications</li>
-                <li><img src="https://img.icons8.com/material/344/gender-neutral-user--v1.png"></img>Profile</li>
-                <li><img src="https://img.icons8.com/material/2x/exit.png"></img>Logout</li>
+                <li onClick={reload}><img src="https://cdn-icons-png.flaticon.com/512/71/71200.png"></img>Jobs</li>
+                <li onClick={navigateToNotifications}><img src="https://cdn-icons-png.flaticon.com/512/565/565422.png"></img>Notifications</li>
+                <li onClick={navigateToProfile}><img src="https://img.icons8.com/material/344/gender-neutral-user--v1.png"></img>Profile</li>
+                <li onClick={logout}><img src="https://img.icons8.com/material/2x/exit.png"></img>Logout</li>
             </ul>
         </div>
     )

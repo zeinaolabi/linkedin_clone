1479a56f4f109230a2fb604b585a2d23e106349a
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-16 10:16:01 +0300

    backend: validate input for job apply api

--- backend/controllers/jobs.controller.js
+++ backend/controllers/jobs.controller.js
@@ -1,8 +1,9 @@
 const Job = require('../models/jobs.model');
 const User = require("../models/users.model");
+const {request, response} = require("express");
 const company_type = 1;
 
-const addJob = async (request, response)=> {
+const addJob = async (request, response) => {
     const {title, description, level, country, company} = request.body;
 
     const checkUserType = await User.findById(company);
@@ -26,7 +27,7 @@ const addJob = async (request, response)=> {
     }
 }
 
-const searchForJob = async (request, response)=> {
+const searchForJob = async (request, response) => {
     const {title} = request.params;
 
     const jobs = await Job.find({title:{'$regex' : title , '$options' : 'i'}})
@@ -36,7 +37,7 @@ const searchForJob = async (request, response)=> {
     return response.status(200).json(jobs);
 }
 
-const getAllJobs = async (request, response)=> {
+const getAllJobs = async (request, response) => {
     const jobs = await Job.find().sort({"createdAt": "desc"});
 
     if(jobs.length === 0) return response.status(200).json({message: "No jobs found"})
@@ -44,8 +45,22 @@ const getAllJobs = async (request, response)=> {
     return response.status(200).json(jobs);
 }
 
+const applyToJob = async (request, response) => {
+    const {userID, jobID} = request.body;
+
+    const checkUser = await User.findById(userID);
+    if(!checkUser) return response.status(404).json({message: "Invalid User"});
+
+    const checkJob = await User.findById(jobID);
+    if(!checkJob) return response.status(404).json({message: "Invalid Job"});
+
+
+
+}
+
 module.exports = {
     searchForJob,
     addJob,
-    getAllJobs
+    getAllJobs,
+    applyToJob
 }
--- backend/routes/jobs.routes.js
+++ backend/routes/jobs.routes.js
@@ -1,8 +1,9 @@
 const {Router} = require('express');
-const {addJob, searchForJob, getAllJobs} = require('../controllers/jobs.controller')
+const {addJob, searchForJob, getAllJobs, applyToJob} = require('../controllers/jobs.controller')
 const router = Router();
 
 router.post('/add_job', addJob);
+router.post('/apply', applyToJob);
 router.get('/search_for_job/:title', searchForJob);
 router.get('/get_all_jobs', getAllJobs);
 

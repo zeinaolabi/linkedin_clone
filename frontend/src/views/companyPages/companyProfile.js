58806ae825209f697b53abf26ffba1364447a81a
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-17 16:34:27 +0300

    frontend: link get jobs per company client

--- frontend/src/views/companyPages/companyProfile.js
+++ frontend/src/views/companyPages/companyProfile.js
@@ -1,29 +1,48 @@
-import React, {useState} from "react";
+import React from "react";
 import { useQuery } from 'react-query';
+import axios from "axios";
 import Header from "./../../components/header/header";
 import Profile from "../../components/profile/profile";
-import axios from "axios";
-const url = "http://127.0.0.1:8000/user/get_profile/" + localStorage.getItem("id");
+import InfoDetails from "../../components/infoDetails/infoDetails";
+const getProfileDataAPI = "http://127.0.0.1:8000/user/get_profile/" + localStorage.getItem("id");
+const getJobsAPI = "http://127.0.0.1:8000/user/get_company_jobs/" + localStorage.getItem("id");
 
 const UserProfilePage = () => {
-    const getData = async () => {
-        const resposne = await axios(url);
+    const getProfileData = async () => {
+        const resposne = await axios(getProfileDataAPI);
         return resposne.data;
     }
 
-    const {data, status} = useQuery('users', getData);
+    const getJobs = async () => {
+        const resposne = await axios(getJobsAPI);
+        return resposne.data;
+    }
+
+    const {data: jobsData, status: jobsStatus} = useQuery('jobs', getJobs);
+    const {data: profileData, status: profileStatus} = useQuery('profile', getProfileData);
 
     return(
         <div className="main_container">
             <Header userType="Company"/>
             <div className="profile_container">
                 <div className="profile_content">
-                    { status === "error" ? <div className="loading">Error Loading</div> : ""}
-                    { status === "loading" ? <div className="loading">Loading..</div> : ""}
+                    { profileStatus === "error" ? <div className="loading">Error Loading</div> : ""}
+                    { profileStatus === "loading" ? <div className="loading">Loading..</div> : ""}
                     {
-                        status === "success" ?
-                        <Profile name={data.name} headline = {data.headline} country={data.country} city={data.city} phone_number={data.phone_number} functionality="Post"/>: ""
+                        profileStatus === "success" ?
+                        <Profile name={profileData.name} headline = {profileData.headline} country={profileData.country} city={profileData.city} phone_number={profileData.phone_number} functionality="Post"/>: ""
                     }
+
+                    <div className="jobs_container">
+                        { jobsStatus === "error" ? <div className="loading">Error Displaying Jobs</div> : ""}
+                        { jobsStatus === "loading" ? <div className="loading">Loading..</div> : ""}
+                        {
+                            jobsStatus === "success" ?
+                            jobsData.map((response)=>{
+                                return(
+                                    <InfoDetails id={response.job._id} title={response.job.title} company_name = {response.company_name} country={response.job.country} date={response.job.createdAt} company={response.job.company}/>
+                                )}) : ""}
+                    </div>
                 </div>
             </div>
         </div>

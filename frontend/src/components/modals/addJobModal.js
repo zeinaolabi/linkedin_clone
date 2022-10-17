e73258d3815590227a04c4bce944ac72ae093e6d
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-10-17 15:46:34 +0300

    frontend: add job modal

---
+++ frontend/src/components/modals/addJobModal.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import axios from "axios";
+const userID = localStorage.getItem("id");
+const addJobAPI = "http://127.0.0.1:8000/jobs/add_job";
+const config = {
+    headers: {
+      Authorization: localStorage.getItem("token")
+    }
+}
+
+const AddJobModal = (props) => {
+    const [input, setInput] = useState({title:"", description:"", country: "", company: userID});
+    const [error, setError ] = useState("");
+
+    const submit = async (e) =>{     
+        await axios.post(addJobAPI, input, config)
+        .then(window.location.reload())
+        .catch(setError("Invalid Input"));
+    }
+
+    if(!props.show){
+        return null
+    }
+    
+    return(
+        <div className="modal" >
+            <div className="modal_content">
+                <div className="modal_header">
+                    <h1>{props.title}</h1>
+                    <button onClick={props.onClose} className="blue_button">Close</button>
+                </div>
+                <div className="modal_body">
+                    <input type="text" className="textfield" placeholder="Title" onChange={(e) =>setInput({...input, email: e.target.value})}></input>
+                    <input type="text" className="textfield" placeholder="Description" onChange={(e) =>setInput({...input, description: e.target.value})}></input>
+                    <input type="text" className="textfield" placeholder="Country" onChange={(e) =>setInput({...input, country: e.target.value})}></input>
+                </div>
+                <div className="modal_footer">
+                    <button onClick={event => submit()} className="blue_btn">Submit</button>
+                    {(error !== "") ? <span className="error_message">{error}</span> : ""}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddJobModal;
